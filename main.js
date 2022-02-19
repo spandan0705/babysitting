edbee0b2e1b5a04f1c9fe220a6b6821639006af7
spandan0705 <85344474+spandan0705@users.noreply.github.com>
2022-02-19 11:34:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,86 @@
+song="";
+object=[];
+status="";
+
+function preload()
+{
+    song=loadSound("music.mp3");
+}
+
+function setup()
+{
+    canvas=createCanvas(600, 500);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log('ModelLoaded');
+    status=true;
+}
+
+function gotResults(error, results)
+{
+    if(error)
+    {
+        console.errror(error);
+    }
+    else{
+        console.log(results);
+        object=results;
+    }
+}
+
+function draw()
+{
+    image(video,0,0,380,380);
+    if(status != "")
+    {
+        objectDetector.detect(video, gotResults);
+
+        for(i = 0; i > object.length; i++)
+        {
+            document.getElementById("status").innerHTML="Status : Object Detected";
+            document.getElementById("number_objects").innerHTML = "Baby Detected";
+
+            if(objects[i].label == "person") 
+            { 
+                document.getElementById("number_of_objects").innerHTML = "Baby Found"; 
+                console.log("stop"); 
+            song.stop(); 
+        
+            if(objects.length == 0) 
+            { 
+              document.getElementById("number_of_objects").innerHTML = "Baby Not Found"; 
+              console.log("play"); song.play();
+             }
+
+            }
+            
+            else 
+            { 
+                document.getElementById("number_of_objects").innerHTML = "Baby Not Found"; 
+                console.log("play"); 
+                song.play(); 
+            }
+        }
+    }
+    else
+    {
+        document.getElementById("status").innerHTML="Status : Object Detected";
+        document.getElementById("number_objects").innerHTML = "Baby Not Detected";
+        song.play();
+    }
+    if(i == 0)
+    {
+        document.getElementById("status").innerHTML="Status : Object Detected";
+        document.getElementById("number_objects").innerHTML = "Baby Not Detected";
+        song.play();
+    }
+}
